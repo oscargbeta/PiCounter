0abf1fb4c4c1afc9ae891cdadaa3cba4f9d061d0
oscargbeta <33643279+oscargbeta@users.noreply.github.com>
2017-11-13 21:22:05 -0400

    Add files via upload

---
+++ src/PiCounter/PiCounter/PiDigitCounter.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace PiCounter
+{
+    partial class PiDigitCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPiDigit = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.labelPiDigitMsg = new System.Windows.Forms.Label();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabelReset = new System.Windows.Forms.LinkLabel();
+            this.panelPiPict = new System.Windows.Forms.Panel();
+            this.labelTotalMsg = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelPiDigit
+            // 
+            this.labelPiDigit.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelPiDigit.AutoSize = true;
+            this.labelPiDigit.BackColor = System.Drawing.Color.Transparent;
+            this.labelPiDigit.Font = new System.Drawing.Font("Calibri", 130F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPiDigit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(217)))), ((int)(((byte)(15)))));
+            this.labelPiDigit.Location = new System.Drawing.Point(705, 95);
+            this.labelPiDigit.Name = "labelPiDigit";
+            this.labelPiDigit.Size = new System.Drawing.Size(143, 213);
+            this.labelPiDigit.TabIndex = 0;
+            this.labelPiDigit.Text = "-";
+            this.labelPiDigit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.BackColor = System.Drawing.Color.Transparent;
+            this.labelTotal.Font = new System.Drawing.Font("Calibri", 130F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(109)))), ((int)(((byte)(17)))));
+            this.labelTotal.Location = new System.Drawing.Point(617, 297);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(178, 213);
+            this.labelTotal.TabIndex = 5;
+            this.labelTotal.Text = "0";
+            this.labelTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelPiDigitMsg
+            // 
+            this.labelPiDigitMsg.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelPiDigitMsg.AutoSize = true;
+            this.labelPiDigitMsg.BackColor = System.Drawing.Color.Transparent;
+            this.labelPiDigitMsg.Font = new System.Drawing.Font("Calibri", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPiDigitMsg.ForeColor = System.Drawing.Color.DarkGray;
+            this.labelPiDigitMsg.Location = new System.Drawing.Point(323, 136);
+            this.labelPiDigitMsg.Name = "labelPiDigitMsg";
+            this.labelPiDigitMsg.Size = new System.Drawing.Size(390, 146);
+            this.labelPiDigitMsg.TabIndex = 7;
+            this.labelPiDigitMsg.Text = "   digit:";
+            this.labelPiDigitMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonMinus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(217)))), ((int)(((byte)(15)))));
+            this.buttonMinus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonMinus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonMinus.FlatAppearance.BorderColor = System.Drawing.Color.ForestGreen;
+            this.buttonMinus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMinus.Font = new System.Drawing.Font("Calibri", 70F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMinus.ForeColor = System.Drawing.Color.White;
+            this.buttonMinus.Location = new System.Drawing.Point(562, 523);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Padding = new System.Windows.Forms.Padding(10, 0, 0, 10);
+            this.buttonMinus.Size = new System.Drawing.Size(220, 136);
+            this.buttonMinus.TabIndex = 2;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = false;
+            this.buttonMinus.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonMinus_MouseDown);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.linkLabelReset);
+            this.panel1.Controls.Add(this.panelPiPict);
+            this.panel1.Controls.Add(this.labelPiDigitMsg);
+            this.panel1.Controls.Add(this.buttonMinus);
+            this.panel1.Controls.Add(this.labelPiDigit);
+            this.panel1.Controls.Add(this.labelTotalMsg);
+            this.panel1.Controls.Add(this.labelTotal);
+            this.panel1.Location = new System.Drawing.Point(4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1056, 683);
+            this.panel1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(217)))), ((int)(((byte)(15)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.CausesValidation = false;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.ForestGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Calibri", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(289, 523);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 10);
+            this.button1.Size = new System.Drawing.Size(220, 136);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonPlus_MouseDown);
+            // 
+            // linkLabelReset
+            // 
+            this.linkLabelReset.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.linkLabelReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabelReset.AutoSize = true;
+            this.linkLabelReset.DisabledLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.linkLabelReset.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelReset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.linkLabelReset.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.linkLabelReset.Location = new System.Drawing.Point(1021, 646);
+            this.linkLabelReset.Name = "linkLabelReset";
+            this.linkLabelReset.Size = new System.Drawing.Size(32, 13);
+            this.linkLabelReset.TabIndex = 3;
+            this.linkLabelReset.TabStop = true;
+            this.linkLabelReset.Text = "reset";
+            this.linkLabelReset.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.linkLabelReset.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelReset_LinkClicked);
+            // 
+            // panelPiPict
+            // 
+            this.panelPiPict.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panelPiPict.BackgroundImage = global::PiCounter.Properties.Resources.pi1;
+            this.panelPiPict.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelPiPict.Location = new System.Drawing.Point(174, 15);
+            this.panelPiPict.Name = "panelPiPict";
+            this.panelPiPict.Size = new System.Drawing.Size(250, 238);
+            this.panelPiPict.TabIndex = 10;
+            // 
+            // labelTotalMsg
+            // 
+            this.labelTotalMsg.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelTotalMsg.AutoSize = true;
+            this.labelTotalMsg.BackColor = System.Drawing.Color.Transparent;
+            this.labelTotalMsg.Font = new System.Drawing.Font("Calibri", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotalMsg.ForeColor = System.Drawing.Color.Gray;
+            this.labelTotalMsg.Location = new System.Drawing.Point(261, 331);
+            this.labelTotalMsg.Name = "labelTotalMsg";
+            this.labelTotalMsg.Size = new System.Drawing.Size(368, 164);
+            this.labelTotalMsg.TabIndex = 6;
+            this.labelTotalMsg.Text = "Total:";
+            this.labelTotalMsg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // PiDigitCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1064, 712);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "PiDigitCounter";
+            this.Text = "Pi Recitation Contest - Digit Counter - v1.0";
+            this.Load += new System.EventHandler(this.PiDigitCounter_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPiDigit;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Label labelPiDigitMsg;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelTotalMsg;
+        private System.Windows.Forms.Panel panelPiPict;
+        private System.Windows.Forms.LinkLabel linkLabelReset;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ src/PiCounter/PiCounter/PiDigitCounter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PiCounter
+{
+    public partial class PiDigitCounter : Form
+    {
+        private int currentCount = 0;
+        private static short totalDigits = 1000; // Upper limit established for PiCounter
+        private static string piStrArray = "3141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643086021394946395224737190702179860943702770539217176293176752384674818467669405132000568127145263560827785771342757789609173637178721468440901224953430146549585371050792279689258923542019956112129021960864034418159813629774771309960518707211349999998372978049951059731732816096318595024459455346908302642522308253344685035261931188171010003137838752886587533208381420617177669147303598253490428755468731159562863882353787593751957781857780532171226806613001927876611195909216420198";
+        private short[] piArray = new short[totalDigits];
+
+        public PiDigitCounter()
+        {
+            InitializeComponent();
+        }
+                
+        private void PiDigitCounter_Load(object sender, EventArgs e)
+        {
+            // Initializing number array and setting the upper limit established for PiCounter.
+            for (int i = 0; i < totalDigits; i++)
+            {
+                piArray[i] = Convert.ToInt16(piStrArray[i].ToString());
+            }
+        }
+
+        private void buttonPlus_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Add to counter
+            if (currentCount < totalDigits)
+            {
+                currentCount++;
+                labelPiDigit.Text = piArray[currentCount - 1].ToString();
+                labelTotal.Text = currentCount.ToString();
+            }
+        }
+
+        private void buttonMinus_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Subtract to counter
+            if (currentCount <= 1)
+            {
+                Reset();
+            }
+            else
+            {
+                currentCount--;
+                labelPiDigit.Text = piArray[currentCount - 1].ToString();
+
+                labelTotal.Text = currentCount.ToString();
+            }
+        }
+
+        private void linkLabelReset_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Reset counter
+            Reset();
+        }
+
+        private void Reset()
+        {
+            currentCount = 0;
+            labelPiDigit.Text = "-";
+            labelTotal.Text = "0";
+        }
+           
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            // In case keyboard keys are being used instead: Add {"Right", "+"}, or Subtract {"Left", "-"}
+            switch (keyData)
+            {
+                case Keys.Right:
+                case Keys.Add:
+                    if (currentCount < totalDigits)
+                    {
+                        currentCount++;
+                        labelPiDigit.Text = piArray[currentCount - 1].ToString();
+                        labelTotal.Text = currentCount.ToString();
+                    }
+                    return true;
+                    break;
+                case Keys.Left:
+                case Keys.Subtract:
+                    if (currentCount <= 1)
+                    {
+                        Reset();
+                    }
+                    else
+                    {
+                        currentCount--;
+                        labelPiDigit.Text = piArray[currentCount - 1].ToString();
+
+                        labelTotal.Text = currentCount.ToString();
+                    }
+                    return true;
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+    }
+}
